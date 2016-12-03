7390a802788f3332ca84eb7f8aee6bfa36b3be55
sokolovs <sokol@zavolga.net>
2016-12-04 01:20:44 +0300

    Fix: field filter for dict or list response with Model item

--- piston/emitters.py
+++ piston/emitters.py
@@ -111,7 +111,7 @@ class Emitter(object):
             if isinstance(thing, QuerySet):
                 ret = _qs(thing, fields=fields)
             elif isinstance(thing, (tuple, list)):
-                ret = _list(thing)
+                ret = _list(thing, fields=fields)
             elif isinstance(thing, dict):
                 ret = _dict(thing, fields=fields)
             elif isinstance(thing, decimal.Decimal):
